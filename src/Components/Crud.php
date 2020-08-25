f036fc595191bd6b8c9d7ce8ca87df592fff2d36
Jan Moorkens <jan.moorkens@artcoresociety.com>
2020-08-25 18:17:42 +0200

    cleaned up Crud.php livewire component
    added this realy cool when clause conditional

--- src/Components/Crud.php
+++ src/Components/Crud.php
@@ -4,7 +4,6 @@ namespace Janmoo\Crudwire\Components;
 use Illuminate\Foundation\Auth\User;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Schema;
 use Janmoo\Crudwire\Traits\GetFillableColumnsTrait;
 
 
@@ -25,21 +24,8 @@ class Crud extends Component
         $this->sortBy           = "id";
         $this->ascDesc          = "asc";
         $this->pagination       = config("crudwire.crudwire_pagination");
-
-
-        $columns = Schema::getColumnListing($user->getTable());
-
-        foreach ($columns as $key => $value) {
-            if (($value === "password") ||($value === "remember_token")) {
-
-                unset($columns[$key]);
-            }
-        }
-
-        $this->columns = $columns;
+        $this->columns          = $this->getFillableColumns($user);
     }
-
-
     /**
      * updatingSearch
      *
@@ -77,21 +63,13 @@ class Crud extends Component
      */
     public function render()
     {
-        $user;
+        $users = User::when($this->search, function($query){
+                    return $query->Where('name', 'like', '%'.$this->search.'%' )
+                    ->orWhere('email', 'like', '%'.$this->search.'%' );
+                })
+                ->orderBy($this->sortBy, $this->ascDesc)
+                ->paginate($this->pagination);
 
-        if ($this->search) {
-            $user = User::Where('name', 'like', '%'.$this->search.'%' )
-                    ->orWhere('email', 'like', '%'.$this->search.'%' )
-                    ->orderBy($this->sortBy, $this->ascDesc)
-                    ->paginate($this->pagination);
-        } else {
-            $user = User::orderBy($this->sortBy, $this->ascDesc)
-                    ->paginate($this->pagination);
-        }
-
-
-        return view('crudwire::crud',[
-            'users' => $user,
-        ]);
+        return view('crudwire::crud',['users' => $users ]);
     }
 }
